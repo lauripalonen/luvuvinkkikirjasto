6b1e4ae6e268e90b3bab3440c84ad2521cc5fce4
Niko Häppölä <niko.happola@gmail.com>
2019-11-20 12:49:16 +0200

    Text based user interface.

--- src/main/java/lukuvinkkikirjasto/Main.java
+++ src/main/java/lukuvinkkikirjasto/Main.java
@@ -1,17 +1,18 @@
 package lukuvinkkikirjasto;
 
 import java.util.Scanner;
+import lukuvinkkikirjasto.UI.UserInterface;
 import lukuvinkkikirjasto.domain.Library;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Lisää linkki: ");
+        Scanner scanner = new Scanner(System.in);
         Library library = new Library(); //in memory
         //Library library = new Library("links.db"); //in sqlite database
-        Scanner scanner = new Scanner(System.in);
-        library.addLink(scanner.nextLine());
+        UserInterface ui = new UserInterface(library, scanner);
 
+        ui.startLibrary();
     }
 
 }
---
+++ src/main/java/lukuvinkkikirjasto/UI/UserInterface.java
@@ -0,0 +1,58 @@
+package lukuvinkkikirjasto.UI;
+
+import java.util.Scanner;
+import lukuvinkkikirjasto.domain.Library;
+
+/**
+ * This class handles text based user interface.
+ */
+public class UserInterface {
+
+    private final Scanner reader;
+    private final Library library;
+
+    public UserInterface(Library library, Scanner reader) {
+        this.library = library;
+        this.reader = reader;
+    }
+
+    /**
+     * Just prints title and "starts" main menu.
+     */
+    public void startLibrary() {
+        System.out.println("Tervetuloa lukuvinkkikirjastoosi!");
+        mainMenu();
+    }
+
+    /**
+     * Main menu, lists possible actions and listens user's choices. Method
+     * calls depend on user inserts.
+     */
+    public void mainMenu() {
+        while (true) {
+            System.out.println("[1] - lisää linkki");
+            //System.out.println("[2] - selaa linkkejä");
+            System.out.println("[x] - lopeta");
+
+            String choice = reader.nextLine();
+            if (choice.equals("1")) {
+                addLink();
+            } else if (choice.equals("2")) {
+                // do something...
+            } else if (choice.equals("x")) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * User interface for adding a new link to library. Simply asks URL of the link.
+     */
+    public void addLink() {
+        System.out.println("Syötä linkin osoite:");
+        String link = reader.nextLine();
+        library.addLink(link);
+        System.out.println();
+    }
+
+}
