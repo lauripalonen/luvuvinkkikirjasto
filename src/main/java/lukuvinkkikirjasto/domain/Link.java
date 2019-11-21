bf1d2a871db13bd04d06faf48840005e8d875a19
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2019-11-21 15:23:52 +0200

    Note and its subclasses Book and Link

--- src/main/java/lukuvinkkikirjasto/domain/Link.java
+++ src/main/java/lukuvinkkikirjasto/domain/Link.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lukuvinkkikirjasto.domain;
 
-/**
- *
- * @author kuokanna
- */
-public class Link {
+public class Link extends Note {
     
+    private String link;
+    
+    public Link(String header, String link) {
+        super(header);
+        this.link = link;
+    }
+    
+    public String getLink() {
+        return this.link;
+    }
 }
