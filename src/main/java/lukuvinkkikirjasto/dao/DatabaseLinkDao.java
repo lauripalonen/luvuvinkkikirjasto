a41dba15d0656b724a8a077fdbb8a7a6654fff41
lauripalonen <palonen.lauri@gmail.com>
2019-11-30 11:24:36 +0200

    import java.net.URI

--- src/main/java/lukuvinkkikirjasto/dao/DatabaseLinkDao.java
+++ src/main/java/lukuvinkkikirjasto/dao/DatabaseLinkDao.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.net.URI;
 import lukuvinkkikirjasto.domain.Book;
 import lukuvinkkikirjasto.domain.Link;
 import lukuvinkkikirjasto.domain.Note;
