a73221c0121a954ec177dbe80f104832123490cf
k0psutin <mr.gravez@gmail.com>
2021-03-05 01:09:41 +0200

    Clean comments and excess imports

--- src/main/java/lukuvinkkikirjasto/Main.java
+++ src/main/java/lukuvinkkikirjasto/Main.java
@@ -1,18 +1,10 @@
 package lukuvinkkikirjasto;
 
-import lukuvinkkikirjasto.domain.Library;
 import lukuvinkkikirjasto.UI.WebUserInterface;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        //Library library = new Library(); //in memory
-        Library library = new Library("links.db"); //in sqlite database
-//        UserInterface ui = new UserInterface(library, scanner);
-//
-//        ui.startLibrary();
-
         WebUserInterface ui = new WebUserInterface();
         ui.start();
     }
